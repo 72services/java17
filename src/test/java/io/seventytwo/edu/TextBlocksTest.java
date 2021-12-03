f1909f951bf7f61a1a5a5e1f5c5abd452075dba4
Simon Martinelli <simon@72.services>
2021-12-03 10:58:24 +0100

    Added formatting test

--- src/test/java/io/seventytwo/edu/TextBlocksTest.java
+++ src/test/java/io/seventytwo/edu/TextBlocksTest.java
@@ -3,6 +3,8 @@ package io.seventytwo.edu;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TextBlocksTest {
 
     @Test
@@ -18,7 +20,7 @@ public class TextBlocksTest {
                 "</body>\n" +
                 "</html>";
 
-        Assertions.assertEquals("<!DOCTYPE html>\n<html>\n<body>\n\n<h1>My First Heading</h1>\n\n<p>My first paragraph.</p>\n\n</body>\n</html>", html);
+        assertEquals("<!DOCTYPE html>\n<html>\n<body>\n\n<h1>My First Heading</h1>\n\n<p>My first paragraph.</p>\n\n</body>\n</html>", html);
     }
 
     @Test
@@ -34,7 +36,7 @@ public class TextBlocksTest {
                 "    </body>\n" +
                 "</html>";
 
-        Assertions.assertEquals("<!DOCTYPE html>\n<html>\n    <body>\n\n        <h1>My First Heading</h1>\n\n        <p>My first paragraph.</p>\n\n    </body>\n</html>", html);
+        assertEquals("<!DOCTYPE html>\n<html>\n    <body>\n\n        <h1>My First Heading</h1>\n\n        <p>My first paragraph.</p>\n\n    </body>\n</html>", html);
     }
 
     @Test
@@ -43,14 +45,24 @@ public class TextBlocksTest {
                 "<html>\n" +
                 "    <body>\n" +
                 "  \n" +
-                "        <h1>My First Heading</h1>\n"+
+                "        <h1>My First Heading</h1>\n" +
                 "  \n" +
                 "        <p>My first paragraph.</p>\n" +
                 "  \n" +
                 "    </body>\n" +
                 "</html>";
 
-        Assertions.assertEquals("<!DOCTYPE html>\n<html>\n    <body>\n  \n        <h1>My First Heading</h1>\n  \n        <p>My first paragraph.</p>\n  \n    </body>\n</html>", html);
+        assertEquals("<!DOCTYPE html>\n<html>\n    <body>\n  \n        <h1>My First Heading</h1>\n  \n        <p>My first paragraph.</p>\n  \n    </body>\n</html>", html);
+    }
+
+    @Test
+    void formatting() {
+        String output = String.format("Name: %s\n" +
+                "Phone: %s\n" +
+                "Address: %s\n" +
+                "Salary: $%.2f\n", "Peter Muster", "032 333 11 22", "Bahnhofstrasse 1, Biel", 70_000.00);
+
+        assertEquals("Name: Peter Muster\nPhone: 032 333 11 22\nAddress: Bahnhofstrasse 1, Biel\nSalary: $70000.00\n", output);
     }
 
 }
