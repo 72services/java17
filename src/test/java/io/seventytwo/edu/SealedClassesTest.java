a691769def06bccc19c02411ae674a1ff7fe923c
Simon Martinelli <simon@72.services>
2021-12-06 07:43:21 +0100

    Added more explanation

--- src/test/java/io/seventytwo/edu/SealedClassesTest.java
+++ src/test/java/io/seventytwo/edu/SealedClassesTest.java
@@ -10,13 +10,23 @@ public class SealedClassesTest {
 
     /**
      * Exercise: A capricorn (Steinbock) is not a pet!
-     * Seal the class pet and only allow cats and dogs
-     * Fix the assertion
+     * Seal the class pet and only allow cats and dogs.
+     * Fix the assertion.
      */
     @Test
     void noPet() {
         Capricorn capricorn = new Capricorn();
 
         assertTrue(capricorn instanceof Pet);
+    }
+
+    /**
+     * Exercise:
+     * Seal all classes and add the correct permitted subclasses.
+     * Add and UnknownAnimal class that permits any subclass.
+     */
+    @Test
+    void unknownAnimal() {
+
     }
 }
