51b760a3f89faa6a75421ef61493f373e3d951ea
Simon Martinelli <simon@72.services>
2021-12-06 07:28:21 +0100

    Added more explanation

--- src/test/java/io/seventytwo/edu/SealedClassesTest.java
+++ src/test/java/io/seventytwo/edu/SealedClassesTest.java
@@ -2,19 +2,21 @@ package io.seventytwo.edu;
 
 import io.seventytwo.edu.animal.Capricorn;
 import io.seventytwo.edu.animal.Pet;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SealedClassesTest {
 
     /**
      * Exercise: A capricorn (Steinbock) is not a pet!
      * Seal the class pet and only allow cats and dogs
+     * Fix the assertion
      */
     @Test
     void noPet() {
         Capricorn capricorn = new Capricorn();
 
-        Assertions.assertTrue(capricorn instanceof Pet);
+        assertTrue(capricorn instanceof Pet);
     }
 }
