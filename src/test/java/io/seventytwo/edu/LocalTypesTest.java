41ba7917d12e460d430adb418f3a2c39779c3c4b
Simon Martinelli <simon@72.services>
2021-12-06 16:28:56 +0100

    Added local record test

--- src/test/java/io/seventytwo/edu/LocalTypesTest.java
+++ src/test/java/io/seventytwo/edu/LocalTypesTest.java
@@ -1,7 +1,30 @@
 package io.seventytwo.edu;
 
+import io.seventytwo.edu.calendar.Day;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Exercise: Write a method that either uses a local record, enum or interface
  */
 public class LocalTypesTest {
+
+    @Test
+    void localRecord() {
+        record DayWithNumberOfLetters(Day day, int numberOfLetters) {
+        }
+
+        List<Day> days = Arrays.stream(Day.values())
+                .map(day -> new DayWithNumberOfLetters(day, day.name().length()))
+                .filter(dayWithNumberOfLetters -> dayWithNumberOfLetters.numberOfLetters > 7)
+                .map(dayWithNumberOfLetters -> dayWithNumberOfLetters.day)
+                .toList();
+
+        assertEquals(3, days.size());
+    }
 }
